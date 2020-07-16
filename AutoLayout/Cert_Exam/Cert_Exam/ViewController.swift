97e7c8eade5c1a25cf7ba70322415178231c57ff
hongsokan_mac <hongsokan@daum.net>
2020-07-16 13:26:25 +0900

    0716 autolayout cert ended

---
+++ AutoLayout/Cert_Exam/Cert_Exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cert_Exam
+//
+//  Created by 안홍석 on 2020/07/15.
+//  Copyright © 2020 안홍석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
