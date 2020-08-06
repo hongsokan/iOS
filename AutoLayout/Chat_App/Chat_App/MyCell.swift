a9bf6423dca8d9cf7a65da4292088f2adc461e88
hongsokan_mac <hongsokan@daum.net>
2020-08-06 15:14:34 +0900

    0805 chat added

---
+++ AutoLayout/Chat_App/Chat_App/MyCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Chat_App
+//
+//  Created by 안홍석 on 2020/08/06.
+//  Copyright © 2020 안홍석. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
