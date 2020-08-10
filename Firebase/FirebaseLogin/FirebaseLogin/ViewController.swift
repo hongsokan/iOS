c5a426c22615edc343e3a2a606d04919faa6c50c
hongsokan_mac <hongsokan@daum.net>
2020-08-10 22:15:42 +0900

    0810 firebase test

---
+++ Firebase/FirebaseLogin/FirebaseLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseLogin
+//
+//  Created by 안홍석 on 2020/08/10.
+//  Copyright © 2020 안홍석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
