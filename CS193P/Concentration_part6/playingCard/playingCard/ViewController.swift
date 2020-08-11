d11180a9f69046a0ee568fe845d00111aef3f7ce
hongsokan_mac <hongsokan@daum.net>
2020-08-11 18:06:51 +0900

    0811 apply .gitignore

---
+++ CS193P/Concentration_part6/playingCard/playingCard/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  playingCard
+//
+//  Created by Kim Taeseon on 2018. 5. 30..
+//  Copyright © 2018년 connect.foundation.sr9872. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var deck = PlayingCardDeck()
+    
+//    [C4-1]
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left, .right]
+            playingCardView.addGestureRecognizer(swipe)
+            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(playingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+            playingCardView.addGestureRecognizer(pinch)
+        }
+    }
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default: break
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
+    }
+
+
+}
+
