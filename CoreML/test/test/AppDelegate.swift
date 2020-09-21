c89fa45592cbacdae20286fa8149439e17f3e688
hongsokan_mac <hongsokan@daum.net>
2020-09-21 11:49:21 +0900

    0921

--- CoreML/test/test/AppDelegate.swift
+++ CoreML/test/test/AppDelegate.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import Fritz
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FritzCore.configure()
+        
         return true
     }
 
